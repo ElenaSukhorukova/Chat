2860abb27470fbec7dce0f24948cde014355fd21
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-25 20:56:21 +0400

    Add locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root 'chatroom#index'
+    get 'login', to: 'session#new'
+  end
 end
