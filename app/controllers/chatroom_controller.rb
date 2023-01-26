5fd925450661ed53849b798a5dbc747aa7f8037d
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-26 21:46:56 +0400

    Add index method to chatroom's controller

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,2 +1,14 @@
 class ChatroomController < ApplicationController
+  before_action :require_authentication
+  before_action :set_user!
+
+  def index
+    @messages = Message.all.includes(:user)
+  end
+
+  private
+
+  def set_user!
+    @user = current_user
+  end
 end
