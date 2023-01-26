373809318cf8755f16365681963031e5ffa2c3b7
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-26 18:30:39 +0400

    Add two helper to Application helper: attribute_model and full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def params_plus(additional_params)
     params.to_unsafe_h.merge(additional_params)
   end
+
+  def attribute_model(model, attr)
+    model.class.human_attribute_name(attr)
+  end
+
+  def full_title(page_title = '')
+    base_title = t('site_name')
+    page_title ? "#{page_title} | #{base_title}" : base_title.to_s
+  end
 end
