4fba82f00b9b1945d9d7b6fa58a63a205c6879d0
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-26 21:50:31 +0400

    Add routes for messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
     root 'chatroom#index'
     
-    resources :users, only: %i[new create edit update]
+    resources :users, only: %i[new create edit update], shallow: true do 
+      resources :messages, only: %i[create]
+    end
+
     resource :session, only: %i[new create destroy]
   end
 end
