d157d9a7b6b9227c7d4b188899679b9c40dc7d99
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-26 18:33:31 +0400

    Add one more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
     root 'chatroom#index'
-    get 'login', to: 'session#new'
+    
+    resources :users, only: %i[new create edit update]
+    resource :session, only: %i[new create destroy]
   end
 end
