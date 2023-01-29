9ec7fb4b1391cd41093fa53f0560f4d3ac8c94d9
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-29 13:50:09 +0400

    Add associations and validates for chatroom's model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,10 @@
 class Chatroom < ApplicationRecord
+validates :chatroom_name, presence: true
+
   belongs_to :user
+  has_many :messages, -> { sorted },  dependent: :destroy
+  has_many :chatroom_users, dependent: :destroy
+  has_many :users, through: :chatroom_users
+
   before_create { self.title = SecureRandom.hex(3) }
 end
