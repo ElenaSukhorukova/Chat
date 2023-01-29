6f6e1754b0906c3bbf47464446a1407c113fa413
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-29 13:40:01 +0400

    Add chatroom's model and chatroom_user's model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
-    root 'chatroom#index'
+    root 'chatrooms#index'
     
     resources :users, only: %i[new create edit update], shallow: true do 
       resources :messages, only: %i[create]
+      resources :chatrooms
     end
 
     resource :session, only: %i[new create destroy]
+
+    mount ActionCable.server, at: '/cable'
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_161257) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_093018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatroom_users", force: :cascade do |t|
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_chatroom_users_on_chatroom_id"
+    t.index ["user_id"], name: "index_chatroom_users_on_user_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "chatroom_name", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_name"], name: "index_chatrooms_on_chatroom_name", unique: true
+    t.index ["user_id"], name: "index_chatrooms_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "body", null: false
@@ -32,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_161257) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "chatrooms", "users"
   add_foreign_key "messages", "users"
 end
