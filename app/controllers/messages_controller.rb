b86552e777b37850ca88f2d6d8702ad1f88e3c02
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-30 15:36:02 +0400

    Update messages' controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,24 @@
 class MessagesController < ApplicationController
   before_action :require_authentication
-  before_action :set_user!
+  before_action :set_variables!
 
   def create
     message = @user.messages.build message_params
 
     if message.save 
-      redirect_to root_path
+      @chatroom.broadcast_append_to @message.chatroom
+      redirect_to chatroom_path(@chatroom)
     end
   end
 
   private
 
-  def set_user!
+  def set_variables!
     @user = current_user
+    @chatroom = Chatroom.find_by chatroom_name: params[:chatroom_id]
   end
 
-  def message_params 
-    params.require(:message).permit(:body)
+  def message_params
+    params.require(:message).permit(:body).merge(chatroom: @chatroom)
   end
 end
