0e6655f22c109c566a91cd7054988b6303b6257f
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-31 13:02:23 +0400

    Fix errors in likes' controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,16 @@ class LikesController < ApplicationController
   
   def create
     message = Message.find params[:message_id]
+    chatroom = message.chatroom
     user = current_user
 
     @like = message.likes.build
     @like.user = user
 
     if @like.save
-      @like.broadcast_append_to "messages_likes"
-      redirect_to chatroom_path(@chatroom)
+      @like.broadcast_append_to @like.message, :likes, target: 'likes',
+      partial: "messages/message", locals: { message: @like.message }
+      redirect_to chatroom_path(chatroom)
     end
   end
 
@@ -17,7 +19,7 @@ class LikesController < ApplicationController
     @like = Like.find params[:id]
     chatroom = @like.message.chatroom
     @like.destroy
-    @like.broadcast_remove_to "messages_likes"
+    @like.broadcast_remove_to @like.message, :likes, target: 'likes'
     redirect_to chatroom_path(chatroom)
   end
 end
