104f13a90946a18b43c18ab3d14a8ad1591156e7
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-31 19:38:14 +0400

    Upgrade an opportunity to do likes of the message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,15 @@
 class MessagesController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :require_authentication
-  before_action :set_variables!
 
   def create
+    @chatroom = Chatroom.find params[:chatroom_id]
+    @user = current_user
     return unless ChatroomUser.where(user: @user, chatroom: @chatroom).exists?
     @message = @user.messages.build message_params
   
     if @message.save 
-      @message.broadcast_append_to @message.chatroom
+      @message.broadcast_append_to  @chatroom, target: "#{dom_id(@chatroom)}_messages"
     else
       @chatroom = Chatroom.find params[:chatroom_id]
       @users = @chatroom.users.order(:user_name)
@@ -17,12 +19,30 @@ class MessagesController < ApplicationController
     end
   end
 
-  private
-
-  def set_variables!
+  def like
+    @message = Message.find params[:id]
+    @chatroom = @message.chatroom
     @user = current_user
-    @chatroom = Chatroom.find params[:chatroom_id]
+    like = @message.likes.find_by(user: @user)
+
+    like.present? ? like.destroy : @message.likes.create(user: @user)
+
+    @message.broadcast_replace_to(
+      [@user, @chatroom],
+      target: "#{dom_id(@message)}_likes", 
+      locals: { message: @message, user: @user },
+      partial: 'messages/heart'
+    )
+
+    @message.broadcast_replace_to(
+      @chatroom,
+      target: "#{dom_id(@message)}_likes_count", 
+      locals: { message: @message },
+      partial: 'messages/likes_count'
+    )
   end
+
+  private
 
   def message_params
     params.require(:message).permit(:body).merge(chatroom: @chatroom)
