062d00a56f605ab95c6eeb8c023ec1993078608d
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-31 11:59:42 +0400

    Edit messages' controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,18 @@ class MessagesController < ApplicationController
   before_action :set_variables!
 
   def create
-    message = @user.messages.build message_params
-
-    if message.save 
-      @chatroom.broadcast_append_to @message.chatroom
+    return unless ChatroomUser.where(user: @user, chatroom: @chatroom).exists?
+    @message = @user.messages.build message_params
+  
+    if @message.save 
+      @message.broadcast_append_to @message.chatroom
       redirect_to chatroom_path(@chatroom)
+    else
+      @chatroom = Chatroom.find params[:chatroom_id]
+      @users = @chatroom.users.order(:user_name)
+      @message ||= @user.messages.build
+      @messages = @chatroom.messages.includes(:user)
+      render('chatrooms/show', status: :unprocessable_entity)
     end
   end
 
@@ -15,7 +22,7 @@ class MessagesController < ApplicationController
 
   def set_variables!
     @user = current_user
-    @chatroom = Chatroom.find_by chatroom_name: params[:chatroom_id]
+    @chatroom = Chatroom.find params[:chatroom_id]
   end
 
   def message_params
