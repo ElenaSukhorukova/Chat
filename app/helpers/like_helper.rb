48d3be5feb6e69d693c65928b65d69ea6b441cb0
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-31 19:38:40 +0400

    Refactoring like's helper

--- app/helpers/like_helper.rb
+++ app/helpers/like_helper.rb
@@ -1,19 +1,9 @@
 module LikeHelper
-  def likes(message)
-    heart_button = content_tag :div, class: "ui labeled button", tabindex: "0" do
-      hart = 
-        if message.likes.find_by(user: @user).present?
-          "🧡"
-        else
-          "🤍"
-        end
-        
-      concat content_tag(:div, hart, class: "ui red button")
-      count = message.likes_count ||= '0'
+  def heart(message, user)
+    message.likes.find_by(user: user).present? ? "🧡" : "🤍"   
+  end
 
-      concat content_tag(:p, count, class: "ui basic red left pointing label")
-    end.html_safe
-
-    heart_button
+  def likes_count(message) 
+    message.likes_count ||= 0
   end
 end
